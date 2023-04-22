48634bdac44628410c0c64d82e92806888e67f8b
tankhun2744 <tankhun@efinancethai.com>
2023-04-22 16:16:26 +0700

    no message

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const routerDB = require('./Route/conDb');
 app.use((req,res, next)=>{
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers','Origin, X-Requseted-With, Content-Type, Accept');
+  next();
 })
 app.get('/', (req, res) => {
   res.json({ message: 'hello!' });
