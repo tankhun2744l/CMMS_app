28194bdc73e6c5c416a6c7c93df9cd996bd2ee0d
tankhun2744 <tankhun@efinancethai.com>
2023-04-22 12:51:21 +0700

    no message

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ app.get('/', (req, res) => {
   res.json({ message: 'hello!' });
 });
 app.use(express.json({limit: '50mb'}))
-app.use(cors())
+app.use(cors({
+  origin: ['*','https://cmms-oa.herokuapp.com/DB/get/status/device/','http://localhost:3000']
+}));
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/DB',routerDB) 
 app.listen(PORT, () => {
   console.log('Application is running on port 5000');
 });
-
