c66e9e21ba1bc6bf73b3628bbed3fa99acf68e6e
tankhun2744 <tankhun@efinancethai.com>
2023-04-29 18:31:08 +0700

    no message

--- server.js
+++ server.js
@@ -7,22 +7,36 @@ cors = require("cors")
 const app = express();
 const routerDB = require('./Route/conDb');
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
-  }
-  next();
-});
+const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+
+app.use(cors(corsOpts));
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   if (req.method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 app.get('/', (req, res) => {
   res.json({ message: 'hello!' });
 });
 app.use(express.json({limit: '50mb'}))
-app.use(cors({
-  origin: ['*','https://cmms-oa.herokuapp.com/DB/get/status/device/','http://localhost:3000']
-}));
+// app.use(cors({
+//   origin: ['*','https://cmms-oa.herokuapp.com/DB/get/status/device/','http://localhost:3000']
+// }));
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/DB',routerDB) 
 app.listen(PORT, () => {
