8e3ad18194ff1def5bf37f104092f2822897847c
tankhun2744 <tankhun@efinancethai.com>
2023-04-29 18:46:12 +0700

    no message

--- server.js
+++ server.js
@@ -2,34 +2,27 @@ const express = require('express')
 const session = require('express-session')
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000
-cors = require("cors")
+// cors = require("cors")
 
 const app = express();
 const routerDB = require('./Route/conDb');
 
-const corsOpts = {
-  origin: '*',
+const cors = require('cors');
+const corsOptions ={
+    origin:'https://inspiring-fox-feafbe.netlify.app', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 
-  methods: [
-    'GET',
-    'POST',
-  ],
-
-  allowedHeaders: [
-    'Content-Type',
-  ],
-};
-
-app.use(cors(corsOpts));
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//   if (req.method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-//     return res.status(200).json({});
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
 app.get('/', (req, res) => {
   res.json({ message: 'hello!' });
 });
@@ -37,6 +30,7 @@ app.use(express.json({limit: '50mb'}))
 // app.use(cors({
 //   origin: ['*','https://cmms-oa.herokuapp.com/DB/get/status/device/','http://localhost:3000']
 // }));
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/DB',routerDB) 
 app.listen(PORT, () => {
